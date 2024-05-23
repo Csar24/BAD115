227e2841e9aa607ea625487ccb3d31248e894b30
Csar24 <gr14079@ues.edu.sv>
2024-05-23 09:22:43 -0600

    Revert "Unificacion de los Proyectos"
    This reverts commit 4b6a8b63fa6641a64350e9ede0aeda2785b60dc0, reversing
    changes made to b25f839c87a04c2c3a5f56012be9c4bf3a2bbf6a.

--- obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d5639e927120208c34a83b1b336caf90f90c17f")]
->>>>>>> 5f2a491fac35e0e85329578513d30165bbd2b242
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
